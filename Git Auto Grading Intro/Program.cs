67974313dc2993a856f695284905b9ea3c151fc0
Emilio Nunez Garay <emilio.nunezgaray@students.snow.edu>
2025-01-23 15:45:14 -0700

    Finished Calculator Project Fixed Problems

--- Git Auto Grading Intro/Program.cs
+++ Git Auto Grading Intro/Program.cs
@@ -10,13 +10,13 @@
             int result1 = Add(2, 3);
             Console.WriteLine("The result of adding 2 and 3 is: " + result1);
 
-            int result2 = Subtract(2, 3);
-            Console.WriteLine("The result of subtracting 2 and 3 is:" + result2);
+            int result2 = Subtract(3, 2);
+            Console.WriteLine("The result of subtracting 3 and 2 is:" + result2);
 
-            int result3 = Subtract(2, 3);
+            int result3 = Multiply(2, 3);
             Console.WriteLine("The result of multiplying 2 and 3 is:" + result3);
 
-            int result4 = Subtract(2, 3);
+            int result4 = (int)Divide(14, 2);
             Console.WriteLine("The result of dividing 2 and 3 is:" + result4);
 
 
