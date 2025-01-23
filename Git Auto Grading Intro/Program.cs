bac86fede4950e8c68e29c61a0cc808fe57e6132
Emilio Nunez Garay <emilio.nunezgaray@students.snow.edu>
2025-01-23 15:35:52 -0700

    Finished Calculator Project

--- Git Auto Grading Intro/Program.cs
+++ Git Auto Grading Intro/Program.cs
@@ -7,30 +7,37 @@
             // Welcome to the calculator project, complete the methods
 
             // Call the methods here ex.
-            int result = Add(2, 3);
-            Console.WriteLine("The result of adding 2 and 3 is: " + result);
+            int result1 = Add(2, 3);
+            Console.WriteLine("The result of adding 2 and 3 is: " + result1);
+
+            int result2 = Subtract(2, 3);
+            Console.WriteLine("The result of subtracting 2 and 3 is:" + result2);
+
+            int result3 = Subtract(2, 3);
+            Console.WriteLine("The result of multiplying 2 and 3 is:" + result3);
+
+            int result4 = Subtract(2, 3);
+            Console.WriteLine("The result of dividing 2 and 3 is:" + result4);
+
+
         }
 
         // Methods Here
         public static int Add(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            return a + b;
         }
         public static int Subtract(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            return a - b;
         }
         public static int Multiply(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
+            return a * b;
         }
         public static decimal Divide(int a, int b)
         {
-            // Code
-            return 0; // Placeholder
-        }
+            return a / b;
     }
+ }
 }
